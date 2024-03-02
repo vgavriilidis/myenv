1f6e1a69a1bbbedf08c3cd0138c9730cba219fb8
Vasileios Gavriilidis <vasilis@mac16.local>
2024-03-02 23:18:17 +0200

    Save Progress

--- install_dev_setup.sh
+++ install_dev_setup.sh
@@ -63,15 +63,15 @@ if [ "$machine" == "Mac" ]; then
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     fi
 
-    # start a server but don't attach to it
-    tmux start-server
-    # create a new session but don't attach to it either
-    tmux new-session -d
-    # install the plugins
-    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-    # killing the server is not required, I guess
-    tmux kill-server
-    tmux source-file ~/.tmux.conf
+    # # start a server but don't attach to it
+    # tmux start-server
+    # # create a new session but don't attach to it either
+    # tmux new-session -d
+    # # install the plugins
+    # ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+    # # killing the server is not required, I guess
+    # tmux kill-server
+    # tmux source-file ~/.tmux.conf
 
 
     if command_exists kitty; then
